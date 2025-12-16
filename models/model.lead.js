e174f8d1e777e0b4fc1397f5b18b347e106e8803
Muhasin141 <muhasinalikhan@gmail.com>
2025-12-16 10:18:56 +0530

    Update model.lead.js

--- models/model.lead.js
+++ models/model.lead.js
@@ -52,7 +52,7 @@ const leadSchema = new mongoose.Schema({
 // Middleware to update the `updatedAt` field on each save
 leadSchema.pre('save', function () {
   this.updatedAt = Date.now();
-  next();
+  
 });
 
 module.exports = mongoose.model('Lead', leadSchema);
